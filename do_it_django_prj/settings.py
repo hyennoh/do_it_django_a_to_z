73010e022eedc5640c9f307269bae03d2c3433bf
Axiomn <worl6109@gmail.com>
2022-12-05 15:38:30 +0900

    Content 입력란에 마크다운 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
-
+    "crispy_forms",
+    "markdownx",
 
     "blog",
     "single_pages",
@@ -118,11 +119,14 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
