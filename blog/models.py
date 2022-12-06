d4736064e7f18cbf83e91711c86aa720bd077b24
Axiomn <worl6109@gmail.com>
2022-12-06 10:16:04 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/models.py
+++ blog/models.py
@@ -63,3 +63,17 @@ class Post(models.Model):
         return self.get_file_name().split('.')[-1]
     def get_content_markdown(self):
         return markdown(self.content)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
