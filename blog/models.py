39ad78d88cd19f5910911b9fc0f25f0b81ad8c32
Axiomn <worl6109@gmail.com>
2022-12-06 20:26:48 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -64,6 +64,12 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1375/9f71ac48368fcf60/svg/{self.author.email}'
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
